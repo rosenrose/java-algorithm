e3dc18f2b212e76126e8e2b74f765b44a57fc227
rosenrose <crazytempler@gmail.com>
2022-10-01 16:32:10 +0900

    tail recursive

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(sumRecur(10));
-        System.out.println(sumRecur(100));
-        System.out.println(sumRecur(1000));
-        System.out.println(sumRecur(10000));
+        System.out.println(sum(10));
+        System.out.println(sum(100));
+        System.out.println(sum(1000));
+        System.out.println(sum(10000));
+        System.out.println(sum(100000));
     }
 
-    private static int sumRecur(int n) {
-        return n <= 1 ? n : n + sumRecur(n - 1);
+    private static int sum(int n) {
+        return sumTailRecur(n, 0);
+    }
+
+    private static int sumTailRecur(int n, int accumulated) {
+        return n <= 0 ? accumulated : sumTailRecur(n - 1, n + accumulated);
     }
 }
