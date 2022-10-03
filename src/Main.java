232bd5a69286160299d2cff1221869ef635f2b27
rosenrose <crazytempler@gmail.com>
2022-10-03 14:54:25 +0900

    Search Rotated Array

--- src/Main.java
+++ src/Main.java
@@ -1,60 +1,37 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(binarySearch(new int[]{1, 2, 3, 4}, 2));
-        System.out.println(binarySearch(new int[]{1, 2, 3, 4, 5}, 4));
-        System.out.println(binarySearch(new int[]{2, 3, 6, 9, 10}, 10));
-        System.out.println(binarySearch(new int[]{1}, 1));
-        System.out.println(binarySearch(new int[]{1}, 3));
-        System.out.println(binarySearch(new int[]{1, 2}, 1));
-        System.out.println(binarySearch(new int[]{1, 2}, 2));
-        System.out.println(binarySearch(new int[]{1, 2}, 3));
-        System.out.println(binarySearchRecursive(new int[]{1, 2, 3, 4}, 0, 3, 2));
-        System.out.println(binarySearchRecursive(new int[]{1, 2, 3, 4, 5}, 0, 4, 4));
-        System.out.println(binarySearchRecursive(new int[]{2, 3, 6, 9, 10}, 0, 4, 10));
-        System.out.println(binarySearchRecursive(new int[]{1}, 0, 0, 1));
-        System.out.println(binarySearchRecursive(new int[]{1}, 0, 0, 3));
-        System.out.println(binarySearchRecursive(new int[]{1, 2}, 0, 1, 1));
-        System.out.println(binarySearchRecursive(new int[]{1, 2}, 0, 1, 2));
-        System.out.println(binarySearchRecursive(new int[]{1, 2}, 0, 1, 3));
+        int[] arr = new int[]{20, 25, 26, 29, 33, 1, 3, 5, 6, 10, 11, 19};
+
+        System.out.println(indexOfRotatedArray(arr, 0, arr.length - 1, 0));
+        System.out.println(indexOfRotatedArray(arr, 0, arr.length - 1, 6));
+        System.out.println(indexOfRotatedArray(arr, 0, arr.length - 1, 20));
+        System.out.println(indexOfRotatedArray(arr, 0, arr.length - 1, 19));
+        System.out.println(indexOfRotatedArray(arr, 0, arr.length - 1, 26));
     }
 
-    private static int binarySearch(int[] arr, int value) {
-        int left = 0;
-        int right = arr.length - 1;
-        int mid;
-
-        while (true) {
-            if (left > right) {
-                return -1;
-            }
-
-            mid = (left + right) / 2;
-
-            if (arr[mid] < value) {
-                left = mid + 1;
-            } else if (arr[mid] > value) {
-                right = mid - 1;
-            } else if (arr[mid] == value) {
-                return mid;
-            }
-        }
-    }
-
-    private static int binarySearchRecursive(int[] arr, int left, int right, int value) {
+    private static int indexOfRotatedArray(int[] arr, int left, int right, int value) {
         if (left > right) {
             return -1;
         }
 
         int mid = (left + right) / 2;
 
-        if (arr[mid] < value) {
-            left = mid + 1;
-        } else if (arr[mid] > value) {
-            right = mid - 1;
-        } else if (arr[mid] == value) {
+        if (arr[mid] == value) {
             return mid;
         }
 
-        return binarySearchRecursive(arr, left, right, value);
+        if (arr[left] <= arr[mid]) {    // left부터 mid까지 정렬된 경우
+            if (arr[left] <= value && value <= arr[mid]) {
+                return indexOfRotatedArray(arr, left, mid - 1, value);
+            }
+
+            return indexOfRotatedArray(arr, mid + 1, right, value);
+        } else {    // mid부터 right까지 정렬된 경우
+            if (arr[mid] <= value && value <= arr[right]) {
+                return indexOfRotatedArray(arr, mid + 1, right, value);
+            }
+
+            return indexOfRotatedArray(arr, left, mid - 1, value);
+        }
     }
 }
