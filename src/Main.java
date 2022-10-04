92ee5f9df30eb39f68ea24d18420f5ce194a1659
rosenrose <crazytempler@gmail.com>
2022-10-04 22:13:59 +0900

    Selection Sort

--- src/Main.java
+++ src/Main.java
@@ -4,29 +4,32 @@ import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < 5; i++) {
             int[] arr = createRandomArray();
-            bubbleSort(arr);
+            selectionSort(arr);
             System.out.println(Arrays.toString(arr));
         }
+
     }
 
     private static int[] createRandomArray() {
         Random rand = new Random();
-        return IntStream.of(new int[rand.nextInt(3, 11)]).map(i -> rand.nextInt(31)).toArray();
+        return IntStream.of(new int[rand.nextInt(1, 11)]).map(i -> rand.nextInt(31)).toArray();
     }
 
-    private static void bubbleSort(int[] arr) {
+    private static void selectionSort(int[] arr) {
         for (int i = 0; i < arr.length - 1; i++) {
-            for (int j = 0; j < arr.length - 1 - i; j++) {  // 한번 훑을 때마다 방문하는 요소 수가 1씩 줄어듦
-                if (arr[j] <= arr[j + 1]) { // 왼쪽 > 오른쪽일 때만 교환하니 안정적
-                    continue;
-                }
+            int minIndex = i;
 
-                int temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
             }
+
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
         }
     }
 }
