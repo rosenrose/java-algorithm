5ab2132dc992a8d47a00802de536920c83b4c1dd
rosenrose <crazytempler@gmail.com>
2022-10-05 01:04:52 +0900

    Quick Sort

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         for (int i = 0; i < 5; i++) {
             int[] arr = createRandomArray();
-            insertionSort(arr);
+            quickSort(arr, 0, arr.length - 1);
             System.out.println(Arrays.toString(arr));
         }
 
@@ -17,17 +17,36 @@ public class Main {
         return IntStream.of(new int[rand.nextInt(1, 11)]).map(i -> rand.nextInt(31)).toArray();
     }
 
-    private static void insertionSort(int[] arr) {
-        for (int i = 1; i < arr.length; i++) {
-            for (int j = i - 1; j >= 0; j--) {
-                if (arr[j] <= arr[j + 1]) {
-                    break;
-                }
-
-                int temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
+    private static void quickSort(int[] arr, int left, int right) {
+        if (left >= right) {
+            return;
         }
+
+        int i = left;
+        int j = right;
+        int pivotValue = arr[(i + j) / 2];
+
+        while (i <= j) {
+            while (arr[i] < pivotValue) {
+                i++;
+            }
+            while (arr[j] > pivotValue) {
+                j--;
+            }
+
+            if (i > j) {
+                break;
+            }
+
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+
+            i++;
+            j--;
+        }
+
+        quickSort(arr, left, j);
+        quickSort(arr, i, right);
     }
 }
